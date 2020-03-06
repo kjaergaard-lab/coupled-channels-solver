387968f562f0171401b7ba73fe3af3dc7b77300b
Ryan Thomas <ryan.j.thomas1@gmail.com>
2020-03-06 17:34:40 +1300

    Partial cross sections now calculated with non-zero mL

--- CalculatePartialCrossSection.m
+++ CalculatePartialCrossSection.m
@@ -8,6 +8,7 @@ InitIdx=FindState(BV(:,3:4),InitState);
 FinalIdx=FindState(BV(:,3:4),FinalState);
 
 InLRange=unique(BV(InitIdx,1));
+InMLRange=unique(BV(InitIdx,2));
 OutLRange=unique(BV(FinalIdx,1));
 OutMLRange=unique(BV(FinalIdx,2));
 
@@ -16,14 +17,20 @@ for n1=1:numel(InLRange)
     for n2=1:numel(InLRange)
         for n3=1:numel(OutLRange)
             for n4=1:numel(OutMLRange)
-                L1=InLRange(n1);
-                L2=InLRange(n2);
-                L=OutLRange(n3);
-                mL=OutMLRange(n4);
-                idx1=FindState(BV,[L1,0,InitState]);
-                idx2=FindState(BV,[L2,0,InitState]);
-                idx3=FindState(BV,[L,mL,FinalState]);
-                cross_sec=cross_sec+squeeze(1i^(L2-L1).*sqrt((2*L1+1).*(2*L2+1)).*conj(T(idx3,idx1,:)).*T(idx3,idx2,:));
+                for n5=1:numel(InMLRange)
+                    for n6=1:numel(InMLRange)
+                        L1=InLRange(n1);
+                        L2=InLRange(n2);
+                        L=OutLRange(n3);
+                        mL=OutMLRange(n4);
+                        mL1=InMLRange(n5);
+                        mL2=InMLRange(n6);
+                        idx1=FindState(BV,[L1,mL1,InitState]);
+                        idx2=FindState(BV,[L2,mL2,InitState]);
+                        idx3=FindState(BV,[L,mL,FinalState]);
+                        cross_sec=cross_sec+squeeze(1i^(L2-L1).*sqrt((2*L1+1).*(2*L2+1)).*conj(T(idx3,idx1,:)).*T(idx3,idx2,:));
+                    end
+                end
             end
         end
     end
