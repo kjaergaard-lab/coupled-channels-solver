fd8d95418e60d7478220021b3afbd8a3ccbd8089
Ryan Thomas <ryan.j.thomas1@gmail.com>
2020-03-30 12:17:07 +1300

    Added 85b87Rb, plotCrossSections

---
+++ plotCrossSections.m
@@ -0,0 +1,19 @@
+clear cs str intState
+
+[~,ia] = unique(BVSymInt(:,3:4),'rows');
+intState = BVSymInt(ia,:);
+jj = 1;
+clf;
+for nn=1:size(intState,1)
+    tmp = CalculatePartialCrossSection(Tsym,BVSymInt,sm.k,InitStateLabel(3:4),intState(nn,3:4));
+    if tmp(end)~=0
+        cs(:,jj) = tmp*(1+1*(intState(nn,3)==intState(nn,4)));
+        plot(sm.E,cs(:,jj),'.-');
+        hold on;
+        str{jj} = sprintf('%d-%d',intState(nn,3:4));
+        jj=jj+1;
+    end
+end
+hold off;
+legend(str);
+        
